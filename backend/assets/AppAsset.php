ba142910cdad5540acf4ae4897f468bcd678becc
MirzabekTUIT <mirzabekshirinov@gmail.com>
2020-11-04 12:02:52 +0500

    first commit

---
+++ backend/assets/AppAsset.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace backend\assets;
+
+use yii\web\AssetBundle;
+
+/**
+ * Main backend application asset bundle.
+ */
+class AppAsset extends AssetBundle
+{
+    public $basePath = '@webroot';
+    public $baseUrl = '@web';
+    public $css = [
+        'css/style.css',
+        'css/mdb.css',
+        'css/bootstrap.css',
+    ];
+    public $js = [
+    ];
+    public $depends = [
+        'yii\web\YiiAsset',
+        'yii\bootstrap\BootstrapAsset',
+    ];
+}
